13e152b9122cac5b5e00462d99c67e690bae6a00
phongvt1712 <phongvtce150200@fpt.edu.vn>
2022-06-28 07:41:18 +0700

    New Android Project

---
+++ Project/app/src/main/java/com/example/food_ordering/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.food_ordering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.ordernow);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
