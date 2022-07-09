24e12d56ab3e09b660b36a7c613385054600e245
NguyenTheTrungCT <TrungNTCE140174@fpt.edu.vn>
2022-07-09 15:44:52 +0700

    ForgetPassword
    ForgotPassword.java line 60 them API neu dung file base con neu dung cong nghe khac se sua lai

--- Project/app/src/main/java/com/example/food_ordering/Login.java
+++ Project/app/src/main/java/com/example/food_ordering/Login.java
@@ -2,7 +2,11 @@ package com.example.food_ordering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Forget password
+        Button buttonForgotPassword = findViewById(R.id.button_forget_password);
+        buttonForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this,"You can reset your password now", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login.this, ForgotPassword.class));
+            }
+        });
+
     }
 }
